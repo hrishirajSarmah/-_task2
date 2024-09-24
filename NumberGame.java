19fdadb01e7cc787804bc190f26e9c1d51492d17
Hrishiraj Sarmah <143607228+hrishirajSarmah@users.noreply.github.com>
2024-09-25 01:19:14 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -21,7 +21,7 @@ public class NumberGame {
                 "2. You will get 5 attempts per round. \n" +
                 "3. Each round has 5 points. If you answer in 1st attempt, you get 5 points, 2nd attempt 4 points and so on. \n" +
                 "4. At the end the total score is calculated. After all rounds. \n" +
-                "5. For each round a new random number is generated between 1-999.");
+                "5. For each round a new random number is generated between 1-100.");
         int roundEntered;
         Scanner roundInput = new Scanner(System.in);
         System.out.println("Enter Number of Rounds: ");
